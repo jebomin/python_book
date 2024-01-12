85e8f30f1d409d4680090874729c4def4bf1e811
jebomin <jebomin0515@naver.com>
2024-01-12 22:55:04 +0900

    문자열 뒤집기

---
+++ 그리디/문자열 뒤집기.py
@@ -0,0 +1,24 @@
+data = input()
+count0 = 0 # 1->0
+count1 = 0 # 0->1
+
+#첫 번째 원소에 대해서 처리
+#이렇게 처리 안해주면 count0이랑 count1이 각각 1로 나옴
+#왜냐? 0001100 입력 값에서 000을 1로 바꾸는걸 신경 안 써주기 때문
+if data[0] == '1':
+  count0 += 1
+else:
+  count1 += 1
+
+#두 번째 원소부터 모든 원소를 확인하며
+for i in range(len(data)-1):
+  if data[i] != data[i+1]:
+    #다음 수에서 1로 바뀌는 경우
+    if data[i+1]=='1':
+      count0 += 1
+    #다음 수에서 0으로 바뀌는 경우
+    else:
+      count1 += 1
+      
+
+print(min(count0, count1))
